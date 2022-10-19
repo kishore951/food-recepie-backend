5efe952ba1f6415076ef1b4be9da5f265b09fb4c
kishore951 <kishoresakthivel951@gmail.com>
2022-10-19 18:17:05 +0530

    new

---
+++ src/main/java/com/nestdigital/foodreceipe/FoodReceipeApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.foodreceipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReceipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReceipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/foodreceipe/FoodReceipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.foodreceipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReceipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
